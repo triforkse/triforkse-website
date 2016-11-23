b697ab701845e08f13d8da9a738fbffacb4af3d7
Thomas Anagrius <thb@trifork.com>
2016-11-23 09:42:28 +0100

    Do some work on the front page.

--- src/splashes/HumioSplash.js
+++ src/splashes/HumioSplash.js
@@ -1,110 +1,69 @@
 import React from 'react';
-import * as d3 from 'd3'
-import Faux from 'react-faux-dom'
 import BEM from '../BEM';
+import Chart from './Chart';
 
 import './HumioSplash.css';
 
 const splash = BEM("splash");
 
 export default React.createClass({
-  mixins: [
-    Faux.mixins.core,
-    Faux.mixins.anim
-  ],
 
   getInitialState () {
     return {
-      chart1: 'loading...',
-      chart2: 'loading...',
-      chart3: 'loading...',
+      ingestionRate: 13.2,
+      compressionRatio: 85.12,
     }
   },
 
   componentDidMount() {
 
-    const update = () => {
-          const chart1 = this.connectFauxDOM('div.humio-chart-1.splash-humio__chart', 'chart1');
-          const chart2 = this.connectFauxDOM('div.humio-chart-2.splash-humio__chart', 'chart2');
-          const chart3 = this.connectFauxDOM('div.humio-chart-3.splash-humio__chart', 'chart3');
+    const updateNumber = () => {
+      this.setState({ingestionRate: (Math.random() * 3 + 12).toFixed(1)});
+    }
 
-          const data = [
-            ["ERROR", 10],
-            ["INFO", 30],
-            ["WARN", 21],
-            ["DEBUG", 4],
-            ["FATAL", 1]
-          ];
+    setInterval(updateNumber, 200);
 
+    const updateRatio = () => {
+      this.setState({compressionRatio: (85 + Math.random()).toFixed(2)});
+    }
 
+    setInterval(updateRatio, 1500);
 
-          var width = 90;
-          var height = 90;
-          var radius = Math.min(width, height) / 2;
-          var color = d3.scaleOrdinal(d3.schemeCategory20b);
-
-          var svg = d3.select(chart1)
-            .append('svg')
-            .attr('width', width)
-            .attr('height', height)
-            .append('g')
-            .attr('transform', 'translate(' + (width / 2) +
-              ',' + (height / 2) + ')');
-
-          var arc = d3.arc()
-            .innerRadius(0)
-            .outerRadius(radius);
-
-          var pie = d3.pie()
-            .value(function(d) { return d.count; })
-            .sort(null);
-
-      var dataset = [
-        { label: 'Abulia', count: Math.random() * 100 + 10 },
-        { label: 'Betelgeuse', count: 20 },
-        { label: 'Cantaloupe', count: 30 },
-        { label: 'Dijkstra', count: 40 }
-      ];
-
-      svg.selectAll('path')
-        .data(pie(dataset))
-        .enter()
-        .append('path')
-        .attr('d', arc)
-        .attr('fill', d => color(d.data.label));
-
-      this.animateFauxDOM(300);
-    };
-
-    update();
-    setInterval(update, 1000);
   },
 
   render() {
-    const chartA = this.state.chart1;
-    const chartB = this.state.chart2;
-    const chartC = this.state.chart3;
+    const ingestionRate = this.state.ingestionRate;
+    const compressionRatio = this.state.compressionRatio;
 
     const chart1 = (
-      <div className='humio-chart-1 splash-humio__chart'>
-        {chartA}
-        <div className="splash-humio__chart-title">Ingestion Rate</div>
+      <div key="humio-chart-1" className='humio-chart-1 splash-humio__chart'>
+        <Chart />
+        <div className="splash-humio__chart-bottom">
+          <div className="splash-humio__chart-title">Real-Time Analysis</div>
+          <div className="splash-humio__chart-timespan">(Past 5m)</div>
+        </div>
       </div>
     );
 
     const chart2 = (
-      <div className='humio-chart-2 splash-humio__chart'>
-        {chartB}
-        <div className="splash-humio__chart-title">Real-Time Analytics</div>
-        <div className="splash-humio__chart-timespan">(Past 5m)</div>
+      <div key="humio-chart-2" className='humio-chart-2 splash-humio__chart'>
+        <div className="splash-humio__value">{ingestionRate}<div className="splash-humio__superscript">MB/s</div></div>
+        <div className="splash-humio__chart-bottom">
+          <div className="splash-humio__chart-title">High Ingestion Rates</div>
+          <div className="splash-humio__chart-timespan">(1.2 TB/day per node)</div>
+        </div>
       </div>
     );
 
     const chart3 = (
-      <div className='humio-chart-3 splash-humio__chart'>
-        {chartC}
-        <div className="splash-humio__chart-title">High Compression</div>
-        <div className="splash-humio__chart-timespan">(Past 24h)</div>
+      <div key="humio-chart-3" className='humio-chart-3 splash-humio__chart'>
+        <div className="splash-humio__compress-outline">
+          <div className="splash-humio__compress"></div>
+        </div>
+        <div className="splash-humio__chart-bottom">
+          <div className="splash-humio__chart-title">High Compression</div>
+          <div className="splash-humio__chart-timespan">(Current Ratio: {compressionRatio}%)</div>
+        </div>
       </div>
     );
 
@@ -125,6 +84,10 @@ export default React.createClass({
           <div {...splash("description")}>
             Use your data as a strategic asset, get real-time insight
             into your business.
+          </div>
+          <div>
+            <a href="#" {...splash("call-to-action")}>Read More</a>
+            <a href="#" {...splash("call-to-action")}>Free 1GB Trial</a>
           </div>
         </div>
       </div>
