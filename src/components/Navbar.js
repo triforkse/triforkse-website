d8aabe12e16b01d1190bd61f2a734ffe94b1714c
Andreas Engl <ae@cephea.de>
2016-11-24 17:36:32 +0100

    Clickable logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,8 +7,9 @@ import './Navbar.css';
 export default (props) => (
   <div className={cn("navbar", {"navbar--transparent": props.transparent})}>
     <div className="navbar__content">
-      <img className="navbar__logo" src={require("../images/logos/trifork_logo.svg")} alt="Trifork - Think Software" />
-
+      <a href='/'>
+        <img className="navbar__logo" src={require("../images/logos/trifork_logo.svg")} alt="Trifork - Think Software" />
+      </a>
 
       <div className="navbar__menu">
         { Menu.main.map(item => <a key={item.title} className="navbar__menu-item" href={item.href}>{item.title}</a>) }
