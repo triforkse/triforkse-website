85972fd7d85cf0da8c50893cd5f3103e55fe01cd
Thomas Anagrius <thb@trifork.com>
2017-02-28 09:39:35 +0100

    Add tech to job page

--- src/components/TeamPage/TeamPage.js
+++ src/components/TeamPage/TeamPage.js
@@ -16,6 +16,25 @@ export default class TeamPage extends Component {
   }
 
   render() {
+    const techName = [
+      {name: "Clojure", description: "Clojure & Datomic"},
+      {name: "Elm", description: "Elm"},
+      {name: "Elixir", description: "Elixir & Phoenix Framework"},
+      {name: "Docker", description: "Docker & Kubernetes"},
+      {name: "Erlang", description: "Erlang"},
+      {name: "GoLang", description: "GoLang"},
+      {name: "Java", description: "Java, Spring & Gradle"},
+      {name: "PostgreSQL", description: "PostgreSQL"},
+      {name: "Python", description: "Python for Machine Learning"},
+      {name: "Kafka", description: "Kafka"},
+      {name: "React", description: "React & React Native"},
+    ];
+
+    const tech = techName.map(t => ({
+      name: t.description,
+      image: require(`./images/${t.name.toLowerCase()}.png`),
+    }))
+
     return (
       <div>
         <Navbar transparent={false} />
@@ -26,17 +45,21 @@ export default class TeamPage extends Component {
               <p>
                 At Trifork we are currently looking for new talent to join in the fun.
                 You think you've got what it takes or are up for the challenge in making us better, you should call us.
-                We think Trifork is a great place to work. That's why we work here! :-)
+                We think Trifork is a great place to work. That is why we work here! :-)
               </p>
             </div>
           </div>
         </div>
-        <div className="section section--content">
+
+        {/* JOB POSTINGS */}
+
+        <div className="segment segment--content">
           <div className="container">
             <div className="row">
               <div className="col-md">
-                <h1 className="section__title">What we're looking for</h1>
-                <div className="section__content">
+                <h1 className="segment__title">What we're looking for</h1>
+                <div className="segment__content">
+                  <div className="description">
                   <p>
                     We are looking for social, outgoing people that consider themselves generalists. You are good at many things and expert at a few things.
                   </p>
@@ -49,16 +72,52 @@ export default class TeamPage extends Component {
                 <p>
                     We are looking for full-stack developers, conference coordinators, and project managers.
                 </p>
+                </div>
               </div>
               </div>
               <div className="col-md">
-                <div className="section__content section__content--no-header">
+                <div className="segment__content segment__content--no-header">
                   <div id="teamtailor-jobs-widget"></div>
                 </div>
               </div>
             </div>
           </div>
         </div>
+
+        {/* TECH */}
+
+        <div className="segment segment--content segment--alternate">
+          <div className="container">
+            <div className="row">
+              <div className="col-md">
+                <h1 className="segment__title">Interested in working at Trifork?</h1>
+                <div className="segment__content">
+                  <p className="description">
+                    Send us an e-mail or swing by at one of our Events.
+                    Most of them we host at the office in Gamla Stan. These events are a great way to learn more
+                    about what it's like to work with us, and you'll get to meet us in person.
+                  </p>
+                </div>
+              </div>
+              <div className="col-md">
+                <div className="segment__content">
+                  <h1 className="segment__title">Tech we choose to work with</h1>
+                  <ul className="icon-list">
+                    {tech.map(t => (
+                      <li className="icon-list__item" key={t.name}>
+                        <div className="icon-list__icon-container">
+                          <img src={t.image} alt={t.name} className="icon-list__icon" />
+                        </div>
+                        <div>{t.name}</div>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
         <PageFooter />
       </div>
     );
