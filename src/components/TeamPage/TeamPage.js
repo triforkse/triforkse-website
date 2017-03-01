a5e14c77ca710955d51d98a8a342777e55a53d56
Thomas Anagrius <thb@trifork.com>
2017-03-01 10:00:26 +0100

    add more text to job page

--- src/components/TeamPage/TeamPage.js
+++ src/components/TeamPage/TeamPage.js
@@ -54,9 +54,9 @@ export default class TeamPage extends Component {
         {/* JOB POSTINGS */}
 
         <div className="segment segment--content">
-          <div className="container">
+          <div className="trifork-container">
             <div className="row">
-              <div className="col-md">
+              <div className="col-xs-12 col-md-6">
                 <h1 className="segment__title">What we're looking for</h1>
                 <div className="segment__content">
                   <div className="description">
@@ -75,7 +75,7 @@ export default class TeamPage extends Component {
                 </div>
               </div>
               </div>
-              <div className="col-md">
+              <div className="col-xs-12 col-md-6">
                 <div className="segment__content segment__content--no-header">
                   <div id="teamtailor-jobs-widget"></div>
                 </div>
@@ -87,9 +87,10 @@ export default class TeamPage extends Component {
         {/* TECH */}
 
         <div className="segment segment--content segment--alternate">
-          <div className="container">
+          <div className="trifork-container">
             <div className="row">
-              <div className="col-md">
+              <div className="col-xs-12 col-md-6">
+                
                 <h1 className="segment__title">Interested in working at Trifork?</h1>
                 <div className="segment__content">
                   <p className="description">
@@ -98,8 +99,39 @@ export default class TeamPage extends Component {
                     about what it's like to work with us, and you'll get to meet us in person.
                   </p>
                 </div>
+
+                <h1 className="segment__title">Interview Process</h1>
+                <div className="segment__content">
+                  <p className="description">
+                    Don't be afraid to apply! We welcome all applications.
+
+                    If you have heard that our interviews are thorough — you heard right — we are tough but fair. We think that finding the right people is the key to success for any company. Our environment has a high level of competence, and we want it to remain that way. We also want to learn new things, so you have to bring something to the table too.
+
+                    We value kind, thoughtful, and considerate people. And our interview process is designed around finding a good fit for the our current team as well.
+                  </p>
+                </div>
+
+                <h1 className="segment__title">Code Retreat</h1>
+                <div className="segment__content">
+                  <p className="description">
+                    A couple of times a year teams rent a car and go out into the wilderness.
+                    Here people have a chance to work on non-client-related projects, hacking on some cool projects together.
+                    Some of our most innovative stuff have resulted from these trips — and that is great fun is just an added bonus!
+                  </p>
+                </div>
+
+                <h1 className="segment__title">Knowledge Sharing</h1>
+                <div className="segment__content">
+                  <p className="description">
+                    At Trifork we believe that it is very important that we always Leaning, Teaching, and Evolving.
+                    We aim to use roughly 10% of our time for sharing and teaching.
+                    For instance we do Tech Lunches — informal ad-hoc events where someone from the team teaches the
+                    others about a e.g. framework, a new language, a hobby project they have been working on.
+                  </p>
+                </div>
+
               </div>
-              <div className="col-md">
+              <div className="col-xs-12 col-md-6">
                 <div className="segment__content">
                   <h1 className="segment__title">Tech we choose to work with</h1>
                   <ul className="icon-list">
